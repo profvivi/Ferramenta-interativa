b8362964014f3d4dfad900d9d402245d0c414d15
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-22 18:54:29 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -1,18 +1,21 @@
 criaCartao(
     'Programação',
     'O que é HTML?',
+    'Abrir',
     'O HTML (Linguagem de Marcação de HiperTexto)'
 )
 
 criaCartao(
     'Programação',
     'O que é CSS?',
+    'Abrir',
     'O CSS é uma linguagem de estilização.'
 )
 
 criaCartao(
     'Programação',
     'O que é JavaScript?',
+    'Abrir',
     'O JavaScript é uma linguagem de programação.'
     
 )
