7d205e062a2779fb97e528d81dd25ca1f727ccc5
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-22 18:58:27 -0300

    s

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function criaCartao(categoria, pergunta, caixa, resposta) {
+function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
@@ -9,9 +9,7 @@ function criaCartao(categoria, pergunta, caixa, resposta) {
         <div class="cartao__conteudo__pergunta">
                 <p>${pergunta}</p>
         </div>
-        <div class="caixa">
-                <p>${Abrir}</p>
-        </div>
+        
         <div class="cartao__conteudo__resposta">
                 <p>${resposta}</p>
         </div>
