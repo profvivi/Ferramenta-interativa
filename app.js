9e1d061e3e98a9a343b15bd619094bd80a8c064c
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-22 17:34:21 -0300

    s

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ function criaCartao(categoria, pergunta, resposta) {
     cartao.className = 'cartao'
 
     cartao.innerHTML = `
-        <div class="cartao__conteudo">
-        <h3>${categoria}</h3>
-        <div class="cartao__conteudo__pergunta">
-                <p>${pergunta}</p>
-        </div>
-        <div class="cartao__conteudo__resposta">
-                <p>${resposta}</p>
-        </div>
-        </div>
-`
+    <div class="cartao__conteudo">
+    <h3>Programação</h3>
+    <div class="cartao__conteudo__pergunta">
+            <p>O que é JavaScript?</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+            <p>O JavaScript é uma linguagem de programação</p>
+    </div>
+    </div>
+    `
 
 container.appendChild(cartao)
 
