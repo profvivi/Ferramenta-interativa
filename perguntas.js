d20036050402388d28825688006115dd5b9b5a2d
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-22 18:22:01 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -18,8 +18,21 @@ criaCartao(
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz OI em inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Programação',
+    'O que é HTML?',
+    'O HTML (Linguagem de Marcação de HiperTexto)'
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação.'
+    
 ) 
 
