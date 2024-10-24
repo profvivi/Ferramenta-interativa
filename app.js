ee2f8629c6834b4186dc9185a939109a65e7aef7
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-24 17:07:09 -0300

    s

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function criaCartao(categoria, pergunta, resposta) {
+function criaCartao(categoria, pergunta, resposta, imagem2) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
